ffe1788690af69e232c4fb099691acef7058fcbc
PieReX777 <jeanpiero.zea@tecsup.edu.pe>
2025-04-03 22:55:30 -0500

    Redirigir al login y mostrar el CRUD de contactos

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return auth()->check() ? redirect()->route('contacts.index') : redirect()->route('login');
+});
+
 use App\Http\Controllers\ContactController;
 
 Route::middleware(['auth'])->group(function () {
